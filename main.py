c98f4c2fe291c6e917392b466df2460149cf0d99
Julia Snabes <julia.snabes@gmail.com>
2023-09-16 14:27:21 -0500

    final version

--- main.py
+++ main.py
@@ -24,6 +24,20 @@ elif fruit_number == 3:
     cart.append(items[2])
     print("You bought 1 orange at $3")
 ask_again = input("Would you like to buy another piece of fruit? y/n ")
+if ask_again == "n":
+    print("  ")
+    print(f"Order summary for {user_name}")
+    count = cart.count("apple")
+    print(count, "apple(s) at $2 each")
+    count2 = cart.count("grape")
+    print(count2, "grape(2) at $1 each")
+    count3 = cart.count("orange")
+    print(count3, "orange(s) at $3 each")
+    total_price = sum(price)
+    print(f"Sub Total: ${total_price}")
+    print(f"5% Tax: ${total_price *0.05}")
+    grand_total = total_price + total_price * 0.05
+    print(f"Total: ${grand_total}")
 
 while ask_again == "y":
     print(f"Welcome {user_name}. Which fruit would you like to buy?")
@@ -41,14 +55,20 @@ while ask_again == "y":
         print("You bought 1 grape at $1")
     elif more_fruit == 3:
         price.append(3)
-        cart.append([items[2]])
+        cart.append(items[2])
         print("You bought 1 orange at $3")
     final_ask = input("Would you like to buy another piece of fruit? y/n ")
     if final_ask == "n":
         print("   ")
         print(f"Order summary for {user_name}")
-        for i in range(len(cart)):
-            print(f"{cart[i]} - ${price[i]}")
+        count = cart.count("apple")
+        print(count, "apple(s) at $2 each")
+        count2 = cart.count("grape")
+        print(count2, "grape(s) at $1 each")
+        count3 = cart.count("orange")
+        print(count3, "orange(s) at $3 each")
+        #for i in range(len(cart)):
+            #print(f"{cart[i]} - ${price[i]}")
         total_price = sum(price)
         print(f"Sub Total: ${total_price}")
         print(f"5% Tax: ${total_price * 0.05}")
